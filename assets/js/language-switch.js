77aac0018d36d65e67351e0597b084ea676b09f7
Nickolai Lisberg Lundby <nickolai.lundby@gmail.com>
2025-03-31 11:38:21 +0200

    more skills

--- assets/js/language-switch.js
+++ assets/js/language-switch.js
@@ -1,16 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const langToggleButton = document.getElementById('lang-toggle');
-    
-    // Check local storage for the saved language
-    const savedLang = localStorage.getItem('language');
-    if (savedLang) {
-      window.location.href = window.location.pathname + "?lang=" + savedLang;
+  const languageToggleButton = document.getElementById('language-toggle');
+  const languageFlag = document.getElementById('language-flag');
+
+  // Check local storage for the saved language
+  const savedLanguage = localStorage.getItem('language');
+  if (savedLanguage) {
+    updateFlagIcon(savedLanguage);
+  } else {
+    // Default to English if no language is saved
+    localStorage.setItem('language', 'en');
+    updateFlagIcon('en');
+  }
+
+  // Toggle language when the button is clicked
+  languageToggleButton.addEventListener('click', function () {
+    const currentLanguage = localStorage.getItem('language');
+    if (currentLanguage === 'en') {
+      localStorage.setItem('language', 'da');
+      updateFlagIcon('da');
+    } else {
+      localStorage.setItem('language', 'en');
+      updateFlagIcon('en');
     }
-  
-    langToggleButton.addEventListener('click', function () {
-      const currentLang = localStorage.getItem('language') || 'en';
-      const newLang = currentLang === 'en' ? 'da' : 'en';
-      localStorage.setItem('language', newLang);
-      window.location.href = window.location.pathname + "?lang=" + newLang;
-    });
+  });
+
+  // Update the flag and text based on the language
+  function updateFlagIcon(language) {
+    if (language === 'da') {
+      languageFlag.classList.remove('fa-flag-uk');
+      languageFlag.classList.add('fa-flag-dk');
+    } else {
+      languageFlag.classList.remove('fa-flag-dk');
+      languageFlag.classList.add('fa-flag-uk');
+    }}
   });
